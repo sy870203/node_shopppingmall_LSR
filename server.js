1059988e4b152224029464ae5cc056d2eef83b71
LEESR <sy870203>
2020-07-29 22:26:45 +0900

    req, res sample test

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const express = require('express');
 
 const app = express();
 
+app.use((req, res) => {
+    res.json({
+        message: "ok"
+    })
+})
+
 const PORT = 8000;
 
 app.listen(PORT, console.log("server started !!"));
